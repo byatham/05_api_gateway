f76b5fea565cebc1400fbed501071ad90c6438c0
byatham <balaiahy23@gmail.com>
2024-10-02 19:45:15 +0530

    first commit

---
+++ src/main/java/com/api/gateway/config/WebConfig.java
@@ -0,0 +1,17 @@
+/*
+ * package com.api.gateway.config;
+ * 
+ * import org.springframework.context.annotation.Configuration; import
+ * org.springframework.web.servlet.config.annotation.CorsRegistry; import
+ * org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ * 
+ * @Configuration public class WebConfig implements WebMvcConfigurer {
+ * 
+ * 
+ * @Override public void
+ * addCorsMappings(org.springframework.web.reactive.config.CorsRegistry
+ * registry) { registry.addMapping("/**") .allowedOrigins("*") // Adjust this to
+ * specify allowed origins .allowedMethods("*") // Adjust this to specify
+ * allowed methods .allowedHeaders("*"); // Adjust this to specify allowed
+ * headers } }
+ */
